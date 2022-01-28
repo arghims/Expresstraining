63fbb0009b576913c30ba8e6c6299f4f58282966
himanshi <agr3600@gmail.com>
2022-01-28 14:53:09 -0800

    loggertask

--- logger.js
+++ logger.js
@@ -1,1 +1,28 @@
 //write logger output to a file
+const express=require("express")
+const fs=require("fs")
+const bodyparser=require("body-parser")
+const app=express()
+app.use(bodyparser.json())
+
+const loggerMw=(req,res,next)=>{
+    console.log(req.method)
+    console.log(req.url)
+    console.log(new Date().toDateString())
+    // let data={
+    //     method:req.method,
+    //     url:req.url
+    // }
+    out = req.method + "\n" + req.url + "\n" + new Date().toDateString()
+    fs.writeFileSync("./public/logger.txt",out.toString())
+   
+    next()
+}
+
+app.get("/logs",loggerMw,(req,res)=>{
+    res.status(200).send("logs")
+})
+
+app.listen(3001,()=>{
+    console.log("server started")
+})
